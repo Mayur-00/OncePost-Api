833b2193879c7dea145ea7df29d606eed2e7f50f
mayur_kendre <kendremayur31@gmail.com>
2026-03-15 16:19:26 +0530

    redo : prisma config for prisma v6.16

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,52 +1,20 @@
-import dotenv from 'dotenv';
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/prisma/client.js';
-
-// Load env variables
-dotenv.config();
+import { PrismaClient } from "../generated/prisma/client.js"
 
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-const connectionString = process.env.DATABASE_URL;
-
-// Debug: Check if URL is loaded
-if (!connectionString) {
-  console.error('❌ DATABASE_URL is not set!');
-  throw new Error('DATABASE_URL environment variable is required');
+  prisma: PrismaClient | undefined
 }
-
-
-const pool = new Pool({
-  connectionString,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
-  max: 10, // max connections
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 5000,
-});
-
-pool.on('error', (err) => {
-  console.error('Unexpected error on idle client', err);
-});
-
-const adapter = new PrismaPg(pool);
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    adapter,
-    log: process.env.NODE_ENV === "development"
-    ? ["query", "info", "warn", "error"]
-    : ["warn", "error"],
-  });
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "info", "warn", "error"]
+        : ["warn", "error"],
+  })
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma
 }
 
-export default prisma;
+export default prisma
