5f59d6832a03789942a99104f0310abad48e6d53
mayur_kendre <kendremayur31@gmail.com>
2026-09-09 14:49:15 +0530

    fix(start): fix start.sh file to make server main process and worker subprocesses

--- start.sh
+++ start.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
 echo "Checking for database migrations..."
-# npx prisma migrate deploy applies any pending migrations in your /prisma/migrations folder
-# It's safe to run even if there are no new migrations.
 npx prisma migrate deploy
 
-echo "Starting OncePost Monolith..."
-
-node --max-old-space-size=256 dist/server.js &
-
-# Start Worker (Foreground)
+# 1. Start Post Worker in the background (&)
 echo "Starting Background Post Worker..."
 node --max-old-space-size=100 dist/workers/post/start-worker.js &
 
+# 2. Start Subscription Worker in the background (&)
 echo "Starting Background Subscription Worker..."
+node --max-old-space-size=92 dist/workers/subscription/start-worker.js &
 
-exec node --max-old-space-size=92 dist/workers/subscription/start-worker.js
+# 3. Start Monolith Server as the MAIN process (exec)
+echo "Starting OncePost Monolith Server..."
+exec node --max-old-space-size=256 dist/server.js
